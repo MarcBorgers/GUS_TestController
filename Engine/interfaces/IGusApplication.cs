e6c545ba1b4cbf27d51dada5cf9a54f4c6f1f8ec
Marc Borgers <marc.borgers.prive@gmail.com>
2015-05-25 11:49:00 +0200

    first test

---
+++ Engine/interfaces/IGusApplication.cs
@@ -0,0 +1,10 @@
+﻿namespace GUS
+{
+    public interface IGusApplication
+    {
+        void Load();
+        string Name { get; }
+        string Driver { get; }
+        IComGusApplication DriverApplication { get; }
+    }
+}
---
+++ Engine/interfaces/IGusSystem.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace GUS
+{
+    public interface IGusSystem
+    {
+        IList<IGusApplication> LoadApps();
+    }
+}
